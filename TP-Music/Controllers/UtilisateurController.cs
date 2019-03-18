1d8438d38b9e8c690abf63a10dc5d4d10c236977
ArthurPlassard1 <45004651+ArthurPlassard1@users.noreply.github.com>
2019-03-18 14:37:06 +0100

    Ajout View Utilisateur

---
+++ TP-Music/Controllers/UtilisateurController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP_Music.Controllers
+{
+    public class UtilisateurController : Controller
+    {
+        // GET: Utilisateur
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            ViewBag.Message = "Menu Utilisateur";
+            
+            return View();
+        }
+    }
+}
